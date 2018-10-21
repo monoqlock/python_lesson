ea6ed992c4990e8baad88971ad4d20d5cfe7ce75
Kaori Morikawa <morikawa_kaori@cyberagent.co.jp>
2018-10-21 18:32:43 +0900

    Initial commit

---
+++ flask-tutorial/README.md
@@ -0,0 +1,2 @@
+# Flask Tutorial (v0.12)
+http://flask.pocoo.org/docs/0.12/tutorial/
---
+++ flask-tutorial/setup.py
@@ -0,0 +1,14 @@
+from setuptools import  setup
+
+setup(
+    name='flaskr',
+    packages=['flaskr'],
+    include_package_data=True,
+    install_reqiores=['flask'],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
